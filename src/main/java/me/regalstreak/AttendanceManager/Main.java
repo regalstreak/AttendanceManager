f8bd70a1ee66a299282a8e507c1a51f8dacab7ca
Neil Agarwal <regalstreak@gmail.com>
2018-10-04 23:15:39 +0530

    AttendanceManager: Delete sample fxml files

--- src/main/java/me/regalstreak/AttendanceManager/Main.java
+++ src/main/java/me/regalstreak/AttendanceManager/Main.java
@@ -10,12 +10,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-
-
         Parent login = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(login));
