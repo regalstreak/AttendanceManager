0cb28005286ef530b02cb636c559b840ff4883b7
Neil Agarwal <regalstreak@gmail.com>
2018-10-05 00:00:41 +0530

    AttendanceManager: Clear up css

--- src/main/java/me/regalstreak/AttendanceManager/Main.java
+++ src/main/java/me/regalstreak/AttendanceManager/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent login = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(login));
         primaryStage.show();
