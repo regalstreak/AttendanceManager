2c7b8d8b6f32112a597b7b74ee3325579c309215
Neil Agarwal <regalstreak@gmail.com>
2018-10-05 02:16:52 +0530

    AttendanceManager: Make those -ox buttons functional

--- src/main/java/me/regalstreak/AttendanceManager/Main.java
+++ src/main/java/me/regalstreak/AttendanceManager/Main.java
@@ -1,16 +1,40 @@
 package me.regalstreak.AttendanceManager;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    private double mouseDragDeltaX;
+    private double mouseDragDeltaY;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent login = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        login.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                mouseDragDeltaX = login.getLayoutX() - mouseEvent.getSceneX();
+                mouseDragDeltaY = login.getLayoutY() - mouseEvent.getSceneY();
+            }
+        });
+
+        login.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                primaryStage.setX(mouseEvent.getScreenX() + mouseDragDeltaX);
+                primaryStage.setY(mouseEvent.getScreenY() + mouseDragDeltaY);
+            }
+        });
 
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(login));
--- src/main/java/me/regalstreak/AttendanceManager/controller/Controller.java
+++ src/main/java/me/regalstreak/AttendanceManager/controller/Controller.java
@@ -1,4 +1,48 @@
 package me.regalstreak.AttendanceManager.controller;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    boolean isFullScreen = false;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    @FXML
+    void close(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void max(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        if (isFullScreen) {
+            stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+            stage.setFullScreen(false);
+            isFullScreen = false;
+        } else {
+            stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+            stage.setFullScreen(true);
+            isFullScreen = true;
+        }
+    }
+
+    @FXML
+    void min(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
 }
